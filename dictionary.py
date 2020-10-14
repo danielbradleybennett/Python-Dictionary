6af9846d686390f3117007ebeabe81d68942cc78
DanielBradleyBennett <danielbradleybennett@gmail.com>
2020-10-13 20:15:28 -0500

    first commit

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+
+# dictionaries (unordered, mutable, does not allow duplicates & indexes w/ key value pairs)
+# create an empty dictionary
+animal = dict()
+# Add k/v pairs
+animal["name"] = "Jonas"
+animal["breed"] = "cat"
+animal["age"] = 666
+
+print(animal)
+
+# create the dictionary with k/v pairs & assign to variable
+
+animal2 = {
+    "name": "Krev",
+    "breed": "Wolf",
+    "age": 35
+}
+
+print(animal2)
